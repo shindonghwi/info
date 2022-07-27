d3b5c74df83bd75850d6ec76bc474db5787a895e
shindonghwi <ehdgnl8940@naver.com>
2022-07-28 00:56:13 +0900

    chore: typescript 적용

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import TextItem from "./components/TextItem";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your ap123p!</Text>
-      <StatusBar style="auto" />
+      <TextItem text="Hello world from React Naitve Web2" />
     </View>
   );
 }
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+
+const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: path.resolve(__dirname, "./public/index.html"),
+  filename: "index.html",
+  inject: "body",
+});
+
+module.exports = {
+  entry: path.join(__dirname, "index.web.js"),
+  output: {
+    filename: "bundle.js",
+    path: path.join(__dirname, "/build"),
+    environment: {
+      arrowFunction: false,
+      bigIntLiteral: false,
+      const: false,
+      destructuring: false,
+      dynamicImport: false,
+      forOf: false,
+      module: false,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules\/(?!()\/).*/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [HTMLWebpackPluginConfig, new ForkTsCheckerWebpackPlugin()],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 9000,
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+    alias: {
+      "react-native$": "react-native-web",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/,
+        exclude: /node_modules\/(?!()\/).*/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
