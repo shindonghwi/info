e4d39f9eecf1fefd048e5e7ac8bafa80ef414b89
shindonghwi <ehdgnl8940@naver.com>
2022-07-28 20:46:36 +0900

    refactor: test code 삭제

--- App.tsx
+++ App.tsx
@@ -1,48 +1,43 @@
 import React from "react";
-import {Platform, StyleSheet, Text, useColorScheme} from "react-native";
-import TextItem from "./components/TextItem";
+import {Platform, Text, useColorScheme} from "react-native";
 import styled, {ThemeProvider} from "styled-components/native"
 import {darkTheme, lightTheme} from "./styles/theme/defaultTheme";
 
-const Header = styled.View`
-  width: 100px;
-  height: 100px;
-  background-color: blue;
-`
-
-interface AA{
-    path: string
-}
-
 const Container = styled.View`
   flex: 1;
+  align-items: center;
+  justify-content: center;
 `
 
-const Poster: React.FC<AA> = ({ path }) => (
-    <Container>
-        <Text> ${path} </Text>
-    </Container>
-)
+const ColorText = styled.Text`
+  font-size: 50px;
+`
+
+interface ContainerProps {
+    os?: string;
+}
+
+const Root: React.FC<ContainerProps> = ({os = Platform.OS}) => {
+
+    const osIsWeb = os === "web"
+
+    return (
+        <Container>
+            <ColorText style={{backgroundColor: osIsWeb ? 'red': 'blue'}}> {os} </ColorText>
+        </Container>
+    )
+}
+
 
 export default function App() {
-    const OS = Platform.OS
     const isDark = useColorScheme() === "dark";
 
     return (
         <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-            <Container>
-                <Poster path={"sdasdfkjjk;sahjksalff"}/>
-                <TextItem text={`${Platform.OS}` + "AA"}/>
-            </Container>
+
+
+
+            <Root/>
         </ThemeProvider>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
