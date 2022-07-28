f04b8851ffcdc3d2dbec92a031213e1ecf02b43e
shindonghwi <ehdgnl8940@naver.com>
2022-07-28 20:14:32 +0900

    feat: typescript - theme ( light, dark ) 모드 분리완료

--- App.tsx
+++ App.tsx
@@ -1,20 +1,48 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {Platform, StyleSheet, Text, useColorScheme} from "react-native";
 import TextItem from "./components/TextItem";
+import styled, {ThemeProvider} from "styled-components/native"
+import {darkTheme, lightTheme} from "./styles/theme/defaultTheme";
+
+const Header = styled.View`
+  width: 100px;
+  height: 100px;
+  background-color: blue;
+`
+
+interface AA{
+    path: string
+}
+
+const Container = styled.View`
+  flex: 1;
+`
+
+const Poster: React.FC<AA> = ({ path }) => (
+    <Container>
+        <Text> ${path} </Text>
+    </Container>
+)
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <TextItem text="Hello SDH1" />
-    </View>
-  );
+    const OS = Platform.OS
+    const isDark = useColorScheme() === "dark";
+
+    return (
+        <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+            <Container>
+                <Poster path={"sdasdfkjjk;sahjksalff"}/>
+                <TextItem text={`${Platform.OS}` + "AA"}/>
+            </Container>
+        </ThemeProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
